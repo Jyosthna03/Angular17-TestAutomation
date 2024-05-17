639d611f4484bae744028b9cc085ea4399d48ac8
jyosthna03 <jenjam@evoketechnologies.com>
2024-05-17 17:29:28 +0530

    commit changes

---
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+template:`
+<div class="bg-danger text-warning">
+    <h2>404 - page Not found</h2>
+    <p>The page you are searching is not found
+</div>`
+})
+
+export class PageNotFoundComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
