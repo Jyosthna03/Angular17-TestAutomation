f6f45e459d7a1ad44b42c096a40f59196fa81a90
Sublari Leela Deepak <leeladeepak123@gmail.com>
2024-06-10 16:23:31 +0530

    changes commit

--- src/app/dashboard/account-details/tabviews/tabSections/recent-transaction/recent-transaction.component.ts
+++ src/app/dashboard/account-details/tabviews/tabSections/recent-transaction/recent-transaction.component.ts
@@ -1,20 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { BankingdataService } from '../../../../../bankingdata.service';
 import { recent } from '../../../../../modal';
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-recent-transaction',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe,CommonModule],
   templateUrl: './recent-transaction.component.html',
   styleUrl: './recent-transaction.component.css'
 })
 export class RecentTransactionComponent {
   constructor(private serv:BankingdataService){}
   RecentTrans!:recent[];
+  dropdownDisabled = true;
   recentTrans = signal(this.RecentTrans);
+  selectOpts = ['Credit Card','Debit Card','Utilities','Mobile Recharge'];
+  selectedOption = 'Credit Card';
 
+  selectOption(option: string) {
+    this.selectedOption = option;
+    console.log(this.selectedOption);
+  }
   ngOnInit(){
     this.serv.getData().subscribe((data:any)=>{
       if(data['RecentTrans'].length!=0){
@@ -23,5 +30,7 @@ export class RecentTransactionComponent {
       }
     });
   }
+
+  
   
 } 
