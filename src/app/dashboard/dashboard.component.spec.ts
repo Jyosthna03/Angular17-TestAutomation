4a38a7e89c7597e216a4301e5556da5d6428b53c
jyosthna03 <jenjam@evoketechnologies.com>
2024-06-26 13:37:00 +0530

    commit

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -29,44 +29,10 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should render the SVG element', () => {
-    const svgElement = fixture.debugElement.query(By.css('svg'));
-    expect(svgElement).toBeTruthy();
-  });
-
-  it('should have correct width and height attributes', () => {
-    const svgElement: SVGElement = fixture.debugElement.query(By.css('svg')).nativeElement;
-    expect(svgElement.getAttribute('width')).toBe('32');
-    expect(svgElement.getAttribute('height')).toBe('32');
-  });
-
-  it('should have path element with correct fill color', () => {
-    const pathElement: SVGPathElement = fixture.debugElement.query(By.css('path')).nativeElement;
-    expect(pathElement.getAttribute('fill')).toBe('#F16A22');
-  });
-
-  it('should render the path element', () => {
-    const pathElement = fixture.debugElement.query(By.css('path'));
-    expect(pathElement).toBeTruthy();
-  });
-
-  it('should have correct attributes for path', () => {
-    const pathElement = fixture.debugElement.query(By.css('path'));
-    expect(pathElement.nativeElement.getAttribute('d')).toBe('M32 0C32 4.2023 31.1723 8.36345 29.5641 12.2459C27.956 16.1283 25.5989 19.6559 22.6274 22.6274C19.6559 25.5989 16.1283 27.956 12.2459 29.5641C8.36345 31.1723 4.2023 32 0 32L1.39876e-06 0H32Z');
-    expect(pathElement.nativeElement.getAttribute('fill')).toBe('#F16A22');
-  });
-
   it('should have a logo class', () => {
     const linkElement = fixture.debugElement.query(By.css('.logo'));
     expect(linkElement).toBeTruthy();
   });
-
-  it('should display LOGO text', () => {
-    const linkElement: HTMLElement = fixture.debugElement.query(By.css('.logo')).nativeElement;
-    expect(linkElement.textContent).toContain('LOGO');
-  });
-
   it('Check the String is correctly Trimmed or not', () => {
     const email = 'leela@gmail.com';
     service.trimmedString = 'Leela';
@@ -75,13 +41,6 @@ describe('DashboardComponent', () => {
     expect(trimmedString).toBeDefined();
     expect(trimName).toEqual(service.trimmedString);
   });
-
-  it('should have correct width and height attributes', () => {
-    const svgElement: SVGElement = fixture.debugElement.query(By.css('#avatar')).nativeElement;
-    expect(svgElement.getAttribute('width')).toBe('40');
-    expect(svgElement.getAttribute('height')).toBe('40');
-  });
-
   it('should contain a button element with "LogOut" text', () => {
     const buttonElement = fixture.debugElement.query(By.css('.btn.btn-primary'));
     expect(buttonElement.nativeElement.textContent.trim()).toBe('LogOut');
